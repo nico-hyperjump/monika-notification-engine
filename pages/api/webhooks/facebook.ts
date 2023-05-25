c8c6c8a03c0042d75c2d4d7bbc1621bb0c97b272
Denny <denny@hyperjump.tech>
2023-05-25 17:29:38 +0700

    Listen to webhook events for WA

--- pages/api/webhooks/facebook.ts
+++ pages/api/webhooks/facebook.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import getRawBody from 'raw-body';
 import { handleErrors } from '../../../services/internal/app-error';
 import logger from '../../../services/internal/logger';
 import { createHmac } from 'crypto';
@@ -33,7 +33,7 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
 
 async function post(req: NextApiRequest, res: NextApiResponse) {
   // Check if x-hub-signature header is present
-  const signatureFromReq = req.headers['X-Hub-Signature'];
+  const signatureFromReq = req.headers['x-hub-signature'];
   if (!signatureFromReq) {
     logger.error('No X-Hub-Signature headers provided');
     return res
@@ -42,9 +42,16 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
   }
 
   // Generate an X Hub Signature using the app secret from the process env
-  const hmac = createHmac('sha256', process.env.APP_SECRET);
-  hmac.update(req.body, 'utf-8');
-  const expectedSignature = `sha256=${hmac.digest('hex')}`;
+  // https://github.com/ilkovich/express-x-hub/blob/master/lib/signature.js
+  const length = req.headers['content-length'];
+  const bodyBuffer = await getRawBody(req, {
+    length,
+    limit: '100kb',
+    encoding: 'utf-8',
+  });
+  const hmac = createHmac('sha1', process.env.APP_SECRET);
+  hmac.update(bodyBuffer);
+  const expectedSignature = `sha1=${hmac.digest('hex')}`;
 
   // Check if signature is valid
   if (expectedSignature !== signatureFromReq) {
@@ -52,11 +59,35 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
     return res.status(403).json({ message: 'Signature does not match' });
   }
 
+  // If valid, parse the raw body to JSON
+  // https://github.com/ilkovich/express-x-hub/blob/master/lib/jparse.js
+  const options = { strict: true };
+  const first = bodyBuffer.trim()[0];
+  if (bodyBuffer.length === 0) {
+    throw new Error('Body is empty');
+  }
+  if (options.strict && first !== '{' && first !== '[') {
+    throw new Error('Not a valid JSON body');
+  }
+
+  let body: string;
+  try {
+    body = JSON.parse(bodyBuffer);
+  } catch (e) {
+    throw new Error('Unable to parse JSON body');
+  }
+
   // If valid, log the body and save it to DB
-  logger.info(`Facebook request body: ${JSON.stringify(req.body)}`);
+  logger.info(`Facebook request body: ${JSON.stringify(body)}`);
   await saveWebhookLog({
-    logs: req.body,
+    logs: body,
   });
 
   return res.status(200).end();
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
